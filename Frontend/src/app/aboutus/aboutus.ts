cf457ea028469a2d514339f8e24b0fe144d2f98b
Dasindu02 <163329767+Dasindu02@users.noreply.github.com>
2025-08-02 22:25:27 +0530

    create  aboutus components

---
+++ Frontend/src/app/aboutus/aboutus.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutus',
+  standalone: true,
+  imports: [],
+  templateUrl: './aboutus.html',
+  styleUrl: './aboutus.scss'
+})
+export class Aboutus {
+
+}
